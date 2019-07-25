7428f58635b172b8bbd13a935ec34148355e7aa8
Valjery W Dorceus <dwelinghton@excilys.com>
2019-07-25 17:00:25 +0200

    added initialization values to database

--- grails-app/init/technight/BootStrap.groovy
+++ grails-app/init/technight/BootStrap.groovy
@@ -1,8 +1,31 @@
 package technight
 
+import technight.domaines.Computer;
+import technight.domaines.Company;
 class BootStrap {
 
     def init = { servletContext ->
+        
+        def companies = []
+        Company asus = new Company(name: 'ASUS')
+        companies.add(asus)
+        Company dell = new Company(name: 'DELL')
+        companies.add(asus)
+        
+        companies.each {company -> company.save(flush: true, failOnError:true)}
+
+        def computers = []
+        Computer rog = new Computer(name: 'Republic OF Gaming', company: asus);
+        computers.add(rog)
+        Computer vivo = new Computer(name: 'Vivo Book', company: asus)
+        computers.add(vivo)
+        Computer latitude = new Computer(name: 'Latitude', company: dell)
+        computers.add(latitude)
+        Computer xps = new Computer(name: 'XPS', company: dell)
+        computers.add(xps)
+
+        computers.each { computer -> computer.save( flush: true, failOnError: true) }
+
     }
     def destroy = {
     }
