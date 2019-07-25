74cea4fdc9b85da2fb99c7b00dfbe46bf0c30f24
Valjery W Dorceus <dwelinghton@excilys.com>
2019-07-25 19:23:32 +0200

    secured Company for Admin only

--- grails-app/controllers/technight/controller/CompanyController.groovy
+++ grails-app/controllers/technight/controller/CompanyController.groovy
@@ -1,7 +1,9 @@
 package technight.controller
 
 import technight.domaines.Company
+import grails.plugin.springsecurity.annotation.Secured
 
+@Secured(["hasRole('ROLE_ADMIN')"])
 class CompanyController {
 
     static scaffold = Company
