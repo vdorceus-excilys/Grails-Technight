7da6ac23b3254813a03f5af8b8dfe4c5d07066b6
Valjery W Dorceus <dwelinghton@excilys.com>
2019-07-25 15:00:32 +0200

    added basic domain's fields

---
+++ grails-app/domain/technight/domaines/Company.groovy
@@ -0,0 +1,9 @@
+package technight.domaines
+
+class Company {
+    
+    String name
+
+    static constraints = {
+    }
+}
---
+++ grails-app/domain/technight/domaines/Computer.groovy
@@ -0,0 +1,11 @@
+package technight.domaines
+
+class Computer {
+
+    String name
+    Date introduced
+    Date discontinued
+
+    static constraints = {
+    }
+}
